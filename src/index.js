2782fb6b52ce809f7d473c6f3555819f9197b370
jeffyuan0420 <jeffyuan0420@gmail.com>
2026-02-26 12:22:23 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+// 這裡的 'root' 必須與 index.html 中的 <div id="root"></div> 完全對應
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("找不到 root 元素，請檢查 index.html 是否包含 <div id='root'></div>");
+}
